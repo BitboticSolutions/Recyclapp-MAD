fa04f42146cf5be0f6a035f0ada7e9a297b548c3
David Rubio <drubio@anasoft.es>
2015-09-30 19:39:50 +0200

    Started the Search

--- RecyclappMAD/app/src/main/java/com/bitbotic/recyclappmad/MainActivity.java
+++ RecyclappMAD/app/src/main/java/com/bitbotic/recyclappmad/MainActivity.java
@@ -2,6 +2,7 @@ package com.bitbotic.recyclappmad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
@@ -28,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
